1340ea553fd49822f4be9c89d2a66c1b020c34f9
DimaP0 <dima.podaryashchiy@gmail.com>
2024-10-06 00:00:27 +0400

    исправлена ошибка падения приложения при входе в аккаунт работника и инженера

--- src/routes/engineerPath.js
+++ src/routes/engineerPath.js
@@ -1,1 +1,13 @@
-export const engineerPath = [];
+import Redirect from "../redirect";
+import Login from "../Login/Login";
+
+export const engineerPath = [
+  {
+    path: "*",
+    element: <Redirect url="/login" />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+];
--- src/routes/workerPath.js
+++ src/routes/workerPath.js
@@ -1,1 +1,13 @@
-export const workerPath = [];
+import Redirect from "../redirect";
+import Login from "../Login/Login";
+
+export const workerPath = [
+  {
+    path: "*",
+    element: <Redirect url="/login" />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+];
